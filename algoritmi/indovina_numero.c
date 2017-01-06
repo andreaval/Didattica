3e09ab597a967d688fd81d6766868959fbef9a4b
andreaval <andrea.vallorani@gmail.com>
2017-01-07 00:15:53 +0100

    Riorganizzata struttura cartelle

---
+++ algoritmi/indovina_numero.c
@@ -0,0 +1,47 @@
+/*
+ * INDOVINA NUMERO
+ * Il computer pensa ad un numero. L'utente deve cercare di indovinarlo.
+ * @author Andrea Vallorani
+ * @version 1.0.0 08/05/2014 17:27
+ */
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+    const int MAX_TENTATIVI = 5;
+    int r,n,mod;
+    int tentativi=0,indovinato=0;
+
+    srand(time(NULL));
+    r=rand()%50;
+    printf("\n* Il computer ha pensato un numero da 0 a 50.\n* Prova ad indovinarlo. Hai a disposizione %d tentativi.\n\n",MAX_TENTATIVI);
+    do{
+        printf("Modalità di gioco (0=easy,1=hard): ");
+        scanf("%d",&mod);
+    }
+    while(mod<0 || mod>1);
+    printf("\n");
+    do{
+        tentativi++;
+        printf("Tentativo %d: ",tentativi);
+        scanf("%d",&n);
+        if(r==n){
+            indovinato=1;
+            printf("Hai indovinato !!");
+        }
+        else{
+            if(mod == 0){
+                if(r>n) printf("Il numero è maggiore di %d\n",n);
+                else printf("Il numero è minore di %d\n",n); 
+            }
+            else printf(":(\n");
+        }
+    }
+    while(!indovinato && tentativi<MAX_TENTATIVI);
+    
+    if(!indovinato){
+    	printf("\nHai perso! Il numero pensato era: %d",r);
+    }
+    printf("\n\n");
+}
---
+++ algoritmi/tabelline.c
@@ -0,0 +1,23 @@
+/**
+ * STAMPA TABELLINE
+ * L'algoritmo stampa le tabelline dal 1 al 10
+ * @author Andrea Vallorani
+ * @version 1.0 19/12/16 22.29
+ */
+#include <stdio.h>
+
+#define FROM 1
+#define TO   10
+
+int main(){
+    int i,t,val;
+    for(i=FROM;i<=TO;i++){
+        val = 0;
+        for(t=1;t<=10;t++){
+            val += i;
+            printf("%3d ",val);
+        }
+        printf("\n");
+    }
+    return 1;
+}
