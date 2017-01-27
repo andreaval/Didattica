762f28cb012626fc23840d00baf5ec849f715f9a
andreaval <andrea.vallorani@gmail.com>
2017-01-27 21:42:51 +0100

    Lettura parametri GET/POST

---
+++ PHP/getpost.php
@@ -0,0 +1,37 @@
+<?php 
+$campi = [];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Controllo dati inviati</title>
+</head>
+<body>
+    Dati inviati tramite:
+<?php
+    if(!empty($_GET)) echo '<strong>GET</strong>';
+    else if(!empty($_POST)) echo '<strong>POST</strong>';
+    else echo '<i>nessun dato inviato</i> (:';
+    
+    if(!empty($_REQUEST)){
+?>
+<h4>Elenco dati inviati:</h4>
+<table border="1">
+    <?php
+    foreach($_REQUEST as $k=>$v){
+        echo "<tr><td><strong>$k</strong></td><td>$v</td></tr>"; 
+    }
+    ?>
+</table>
+<?php } 
+$errori = [];
+foreach($campi as $k){
+    if(!isset($_REQUEST[$k])) $errori[]=$k;
+}
+if(count($errori)){
+    echo '<h4 style="color:red">Non hai inviato i seguenti campi: '.implode(' , ',$errori).'</h4>';
+}
+else if(count($campi)) echo '<h4 style="color:green">OK tutti i campi richiesti sono stati inviati</h4>';
+?>
+</body>
+</html>
