0c4788665f6b8eea131c656aa90e323e8cb65d7b
andreaval <andrea.vallorani@gmail.com>
2017-02-13 00:55:25 +0100

    Esempio interruzione script

---
+++ PHP/exit_die.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP: comandi exit e die</title>
+</head>
+<body>
+    <?php
+        exit('Interrompo esecuzione script');
+        //die('Interrompo esecuzione script'); //die è equivalente al comando exit
+    ?>
+    Testo pagina
+</body>
+</html>
