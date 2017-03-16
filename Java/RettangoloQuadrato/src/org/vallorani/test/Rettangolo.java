48e2daf4ac3bbc43a1ad7c15cb05ab9dceb02570
andreaval <andrea.vallorani@gmail.com>
2017-03-16 17:38:13 +0100

    migliorati i commenti

--- Java/RettangoloQuadrato/src/org/vallorani/test/Rettangolo.java
+++ Java/RettangoloQuadrato/src/org/vallorani/test/Rettangolo.java
@@ -1,39 +1,48 @@
 package org.vallorani.test;
 
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
+import javax.media.j3d.Geometry;
+
 /**
  * Classe Rettangolo
+ *
  * @author Andrea Vallorani
  */
 public class Rettangolo {
-    
+
     /**
      * Misure dei lati del rettangolo
      */
-    private double l1,l2;
-    
+    private double l1, l2;
+
     /**
      * Metodo costruttore
+     *
      * @param x Misura da assegnare a due lati del rettangolo
      * @param y Misura da assegnare agli altri due lati del rettangolo
      */
-    public Rettangolo(double x,double y){
+    public Rettangolo(double x, double y) {
         this.l1 = x;
         this.l2 = y;
     }
-    
+
     /**
      * Calcolo area del rettangolo
-     * @return valore area 
+     *
+     * @return area
      */
-    public double calcolaArea(){
-        return this.l1*this.l2;
+    public double calcolaArea() {
+        return this.l1 * this.l2;
     }
-    
+
     /**
      * Calcolo perimetro del rettangolo
-     * @return valore perimetro
+     *
+     * @return perimetro
      */
-    public double calcolaPerimetro(){
-        return (this.l1*2)+(this.l2*2);
+    public double calcolaPerimetro() {
+        return (this.l1 * 2) + (this.l2 * 2);
     }
 }
