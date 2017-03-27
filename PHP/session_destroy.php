02b030e5811bd8f1ca07140e5644af60fa117b50
andreaval <andrea.vallorani@gmail.com>
2017-03-27 22:52:52 +0200

    Chiusura di una sessione web

--- PHP/session_destroy.php
+++ PHP/session_destroy.php
@@ -2,7 +2,7 @@
 /**
  * Esempio di chiusura di una sessione web in PHP
  * @author Andrea Vallorani
- * @version 1.0 2017-03-27 19:51
+ * @version 1.0 2017-03-27 19:52
  */
  
 //imposto il nome della sessione
