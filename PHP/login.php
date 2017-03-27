42fed8d06dc3996a1e3403051c40a2e2961198df
andreaval <andrea.vallorani@gmail.com>
2017-03-27 22:54:03 +0200

    Procedura di autenticazione username+password

--- PHP/login.php
+++ PHP/login.php
@@ -2,7 +2,7 @@
 /**
  * Esempio di pagina di autenticazione
  * @author Andrea Vallorani
- * @version 1.0 2017-03-17 21:31
+ * @version 1.0 2017-03-17 21:32
  */
 
 //apro la sessione (molto probabilmente sarà una nuova sessione)
