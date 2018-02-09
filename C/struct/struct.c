3a0191ac45e279062853563899bc91127599e8c3
andreaval <andrea.vallorani@gmail.com>
2018-02-09 19:45:55 +0100

    Utilizzo base delle strutture

--- C/struct/struct.c
+++ C/struct/struct.c
@@ -1,6 +1,6 @@
 /**
  * Utilizzo base delle struct
- * @author Andrea Vallorani - 2018/02/05 18:59
+ * @author Andrea Vallorani - 2018/02/09 19:39
  */
 #include <stdio.h>
 #include <string.h>
@@ -26,6 +26,13 @@ int main(){
     book.id = 1;
     //dichiaro book2
     struct Books book2 = {"Harry Potter","J. K. Rowling",2};
+    //dichiaro array di book
+    struct Books books[10];
+    printf("Inserisci titolo libro: ");
+    scanf("%s",books[0].title);
+    printf("Inserisci autore libro: ");
+    scanf("%s",books[0].author);
+    books[0].id=3;
     //dichiaro contact
     Contact contact = {"mario","333123456",1};
     //dichiaro un puntatore alla struttura
@@ -33,6 +40,7 @@ int main(){
     //stampo le strutture
     printf("Libro: %s di %s (%d)\n",book.title,book.author,book.id);
     printf("Libro: %s di %s (%d)\n",book2.title,book2.author,book2.id);
+    printf("Libro: %s di %s (%d)\n",books[0].title,books[0].author,books[0].id);
     printf("Contatto: %s tel.%s (%d)\n",contact.name,contact.phone,contact.id);
     printf("Contatto: %s tel.%s (%d)\n",copy->name,copy->phone,copy->id);
 }
